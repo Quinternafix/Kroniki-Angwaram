36d03445f1a3e751c568f06bc518878c400fa8fe
Quinternafix <quint4022@gmail.com>
2026-07-27 10:12:16 +0200

    Add files via upload

--- js/router.js
+++ js/router.js
@@ -2,83 +2,12 @@ import { homeView } from "./views/home.js";
 import { charactersView } from "./views/characters.js";
 import { profileView } from "./views/profile.js";
 import { factionsView } from "./views/factions.js";
+import { factionView } from "./views/faction.js";
 import { placesView } from "./views/places.js";
+import { placeView } from "./views/place.js";
 import { timelineView } from "./views/timeline.js";
 import { notFoundView } from "./views/notfound.js";
-import { factionView } from "./views/faction.js";
-import { placeView } from "./views/place.js";
 
-export async function router() {
-
-    const app = document.getElementById("app");
-
-    const hash = location.hash || "#/";
-
-    if (hash.startsWith("#/characters/")) {
-
-        const id = hash.split("/")[2];
-
-        app.innerHTML = await profileView(id);
-
-        return;
-
-    }
-
-    switch (hash) {
-
-        case "#/":
-
-            app.innerHTML = homeView();
-
-            break;
-
-        case "#/characters":
-
-            app.innerHTML = await charactersView();
-
-            break;
-
-        case "#/characters/morgath":
-
-            app.innerHTML = await profileView("morgath");
-
-            break;
-
-        case "#/factions":
-
-            app.innerHTML = factionsView();
-
-            break;
-
-        case "#/factions/zjednoczone królestwo selidoru":
-
-            app.innerHTML = await profileView("zjednoczone królestwo selidoru");
-
-            break;
-
-        case "#/places":
-
-            app.innerHTML = placesView();
-
-            break;
-
-        case "#places/selidor":
-
-            app.innerHTML = await profileView("selidor");
-
-            break;
-
-        case "#/timeline":
-
-            app.innerHTML = timelineView();
-
-            break;
-
-        default:
-
-            app.innerHTML = notFoundView();
-
-    }
 export async function router() {
     const app = document.getElementById("app");
     const hash = location.hash || "#/";
@@ -120,5 +49,4 @@ export async function router() {
         default:
             app.innerHTML = notFoundView();
     }
-}
 }
