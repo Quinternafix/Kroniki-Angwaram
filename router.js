1766a223ac2b9975db8d7a40980f6520169366c8
Quinternafix <quint4022@gmail.com>
2026-07-27 10:09:45 +0200

    Add files via upload

--- router.js
+++ router.js
@@ -2,47 +2,51 @@ import { homeView } from "./views/home.js";
 import { charactersView } from "./views/characters.js";
 import { profileView } from "./views/profile.js";
 import { factionsView } from "./views/factions.js";
+import { factionView } from "./views/faction.js";
 import { placesView } from "./views/places.js";
+import { placeView } from "./views/place.js";
 import { timelineView } from "./views/timeline.js";
 import { notFoundView } from "./views/notfound.js";
 
 export async function router() {
-
     const app = document.getElementById("app");
+    const hash = location.hash || "#/";
 
-    if (location.hash.startsWith("#/characters/")) {
-
-        const id = location.hash.split("/")[2];
-
+    if (hash.startsWith("#/characters/")) {
+        const id = hash.split("/")[2];
         app.innerHTML = await profileView(id);
-
         return;
     }
 
-    switch (location.hash || "#/") {
+    if (hash.startsWith("#/factions/")) {
+        const id = hash.split("/")[2];
+        app.innerHTML = await factionView(id);
+        return;
+    }
 
+    if (hash.startsWith("#/places/")) {
+        const id = hash.split("/")[2];
+        app.innerHTML = await placeView(id);
+        return;
+    }
+
+    switch (hash) {
         case "#/":
             app.innerHTML = homeView();
             break;
-
         case "#/characters":
             app.innerHTML = await charactersView();
             break;
-
         case "#/factions":
-            app.innerHTML = factionsView();
+            app.innerHTML = await factionsView();
             break;
-
         case "#/places":
-            app.innerHTML = placesView();
+            app.innerHTML = await placesView();
             break;
-
         case "#/timeline":
             app.innerHTML = timelineView();
             break;
-
         default:
             app.innerHTML = notFoundView();
-            break;
     }
 }
