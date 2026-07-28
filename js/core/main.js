cc7747435fca6ff8364c6caa547543366eb809b0
Quinternafix <quint4022@gmail.com>
2026-07-28 22:04:48 +0200

    Update main.js

--- js/core/main.js
+++ js/core/main.js
@@ -1,1 +1,12 @@
+import { router } from "./router.js";
+import { initI18n } from "./core/i18n.js";
+import { initSearch } from "./core/search.js";
 
+window.addEventListener("DOMContentLoaded", () => {
+    initI18n();
+    initSearch();
+    router();
+});
+
+window.addEventListener("hashchange", router);
+window.addEventListener("languagechange", router);
