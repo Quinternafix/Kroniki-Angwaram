b9ad10162a0303c55f1de54b1bf8ad96a09a904e
Quinternafix <quint4022@gmail.com>
2026-07-28 21:43:46 +0200

    Update sidebar.js

--- components/sidebar.js
+++ components/sidebar.js
@@ -2,15 +2,11 @@ export function renderSidebar() {
 
 document.getElementById("sidebar").innerHTML = `
 
-<a href="#/">🏠 Start</a>
-
-<a href="#/characters">👤 Postacie</a>
-
-<a href="#/factions">⚔ Frakcje</a>
-
-<a href="#/places">🏰 Miejsca</a>
-
-<a href="#/timeline">📜 Historia</a>
+<a href="#/">🏠 ${t("nav.home")}</a>
+<a href="#/characters">👤 ${t("nav.characters")}</a>
+<a href="#/factions">⚔ ${t("nav.factions")}</a>
+<a href="#/places">🏰 ${t("nav.places")}</a>
+<a href="#/timeline">📜 ${t("nav.timeline")}</a>
 
 `;
 
