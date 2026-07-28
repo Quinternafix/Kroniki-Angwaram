2fca84ca8cd81e57feb009a00c48e48c5c102a93
Quinternafix <quint4022@gmail.com>
2026-07-28 21:58:32 +0200

    Update search.js

--- js/core/search.js
+++ js/core/search.js
@@ -1,17 +1,13 @@
 import { state } from "../state.js";
 
-export function initSearch(){
+export function initSearch() {
+    window.addEventListener("DOMContentLoaded", () => {
+        const input = document.getElementById("searchInput");
+        if (!input) return;
 
-    const input = document.getElementById("searchInput");
-
-    if(!input) return;
-
-    input.addEventListener("input",()=>{
-
-        state.search=input.value.toLowerCase();
-
-        window.dispatchEvent(new Event("hashchange"));
-
+        input.addEventListener("input", () => {
+            state.search = input.value.toLowerCase();
+            window.dispatchEvent(new Event("hashchange"));
+        });
     });
-
 }
