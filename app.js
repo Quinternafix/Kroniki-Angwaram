dd8d7e8ed2f32de0b40a12a0a7667c94e38459cd
Quinternafix <quint4022@gmail.com>
2026-07-30 19:34:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import { renderNavbar } from "./components/navbar.js";
 import { renderSidebar } from "./components/sidebar.js";
 import { renderFooter } from "./components/footer.js";
+import { initI18n } from "./core/i18n.js";
 import { router } from "./router.js";
 
 renderNavbar();
 renderSidebar();
 renderFooter();
 
-window.addEventListener("load", router);
+initI18n();
+
 window.addEventListener("hashchange", router);
+
+router();
