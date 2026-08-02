4835a5e0d5c7e170aa16b7439c35c7db1062b79a
Quinternafix <quint4022@gmail.com>
2026-08-02 22:00:16 +0200

    Update api.js

--- js/core/api.js
+++ js/core/api.js
@@ -1,17 +1,25 @@
+import { t } from "./i18n.js";
+
 const cache = {};
 
 export async function getData(file) {
+
     if (cache[file]) {
         return cache[file];
     }
 
-    const response = await fetch(`data/${file}.json`);
+    const response = await fetch(
+        `assets/data/${file}.json`
+    );
 
     if (!response.ok) {
-        throw new Error(t("error.notFoundFile") + ` data/${file}.json`);
+        throw new Error(
+            `${t("error.notFoundFile")} assets/data/${file}.json`
+        );
     }
 
     const data = await response.json();
+
     cache[file] = data;
 
     return data;
