2cb363cc7f337fd2873062ef71c074d27d40f251
Quinternafix <quint4022@gmail.com>
2026-08-07 08:30:19 +0200

    Update sidebar.js

--- js/components/sidebar.js
+++ js/components/sidebar.js
@@ -1,17 +1,61 @@
+import { t } from "../core/i18n.js";
+
 export function renderSidebar() {
 
-document.getElementById("sidebar").innerHTML = `
+    const sidebar = document.getElementById("sidebar");
 
-<a href="#/">🏠 Start</a>
+    if (!sidebar) {
+        return;
+    }
 
-<a href="#/characters">👤 Postacie</a>
+    sidebar.innerHTML = `
 
-<a href="#/factions">⚔ Frakcje</a>
+        <nav class="sidebar-nav">
 
-<a href="#/places">🏰 Miejsca</a>
+            <a href="#/">
+                🏠
+                <span data-i18n="nav.home">
+                    ${t("nav.home")}
+                </span>
+            </a>
 
-<a href="#/timeline">📜 Historia</a>
+            <a href="#/characters">
+                👤
+                <span data-i18n="nav.characters">
+                    ${t("nav.characters")}
+                </span>
+            </a>
 
-`;
+            <a href="#/factions">
+                ⚔
+                <span data-i18n="nav.factions">
+                    ${t("nav.factions")}
+                </span>
+            </a>
+
+            <a href="#/places">
+                🏰
+                <span data-i18n="nav.places">
+                    ${t("nav.places")}
+                </span>
+            </a>
+
+            <a href="#/timeline">
+                📜
+                <span data-i18n="nav.timeline">
+                    ${t("nav.timeline")}
+                </span>
+            </a>
+
+            <a href="#/library">
+                📚
+                <span data-i18n="nav.library">
+                    ${t("nav.library")}
+                </span>
+            </a>
+
+        </nav>
+
+    `;
 
 }
