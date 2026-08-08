27679c44bf903b3c64b30fdca0172d06d57bcfaf
Quinternafix <quint4022@gmail.com>
2026-08-08 12:59:42 +0200

    Update footer.js

--- js/components/footer.js
+++ js/components/footer.js
@@ -1,5 +1,16 @@
+import { t } from "../core/i18n.js";
+
 export function renderFooter() {
 
-document.getElementById("footer").innerHTML = `
-© 2026 ${t("site.title")}
-`;
+    const footer = document.getElementById("footer");
+
+    if (!footer) {
+        return;
+    }
+
+    footer.innerHTML = `
+        <span>
+            © 2026 ${t("site.title")}
+        </span>
+    `;
+}
