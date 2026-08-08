896af612898f4eaa168ff05d365573632c3497b7
Quinternafix <quint4022@gmail.com>
2026-08-08 13:13:30 +0200

    Update sidebar.js

--- js/components/sidebar.js
+++ js/components/sidebar.js
@@ -2,7 +2,8 @@ import { t } from "../core/i18n.js";
 
 export function renderSidebar() {
 
-    const sidebar = document.getElementById("sidebar");
+    const sidebar =
+        document.getElementById("sidebar");
 
     if (!sidebar) {
         return;
@@ -13,49 +14,66 @@ export function renderSidebar() {
         <nav class="sidebar-nav">
 
             <a href="#/">
+
                 🏠
+
                 <span data-i18n="nav.home">
                     ${t("nav.home")}
                 </span>
+
             </a>
 
             <a href="#/characters">
+
                 👤
+
                 <span data-i18n="nav.characters">
                     ${t("nav.characters")}
                 </span>
+
             </a>
 
             <a href="#/factions">
+
                 ⚔
+
                 <span data-i18n="nav.factions">
                     ${t("nav.factions")}
                 </span>
+
             </a>
 
             <a href="#/places">
+
                 🏰
+
                 <span data-i18n="nav.places">
                     ${t("nav.places")}
                 </span>
+
             </a>
 
             <a href="#/timeline">
+
                 📜
+
                 <span data-i18n="nav.timeline">
                     ${t("nav.timeline")}
                 </span>
+
             </a>
 
             <a href="#/library">
+
                 📚
+
                 <span data-i18n="nav.library">
                     ${t("nav.library")}
                 </span>
+
             </a>
 
         </nav>
 
     `;
-
 }
