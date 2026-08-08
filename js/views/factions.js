5c9f21e58b0e72e91694c23b2a795aa46fb0e04a
Quinternafix <quint4022@gmail.com>
2026-08-08 13:07:08 +0200

    Update factions.js

--- js/views/factions.js
+++ js/views/factions.js
@@ -2,10 +2,62 @@ import { getData } from "../core/api.js";
 import { localize, t } from "../core/i18n.js";
 
 export async function factionsView() {
-  const factions = await getData("factions");
-  return `<h1>${t("factions.title")}</h1><div class="cards-grid">${factions.map(faction => `<article class="card">
-    <img src="${faction.image}" alt="${localize(faction, "name")}" class="card-image">
-    <h2>${localize(faction, "name")}</h2><p>${localize(faction, "description")}</p>
-    <a href="#/factions/${faction.id}" class="card-button">${t("common.open")}</a>
-  </article>`).join("")}</div>`;
+
+    const factions = await getData("factions");
+
+    return `
+
+        <section class="factions-page">
+
+            <h1>${t("factions.title")}</h1>
+
+            <div class="cards-grid">
+
+                ${factions.map(faction => {
+
+                    const name = localize(faction, "name");
+                    const description = localize(faction, "description");
+
+                    return `
+
+                        <article class="card">
+
+                            <img
+                                src="${faction.image || ""}"
+                                alt="${name}"
+                                class="card-image"
+                            >
+
+                            <div class="card-content">
+
+                                <h2>
+                                    ${name}
+                                </h2>
+
+                                ${
+                                    description
+                                        ? `<p>${description}</p>`
+                                        : ""
+                                }
+
+                                <a
+                                    href="#/factions/${encodeURIComponent(faction.id)}"
+                                    class="card-button"
+                                >
+                                    ${t("common.open")}
+                                </a>
+
+                            </div>
+
+                        </article>
+
+                    `;
+
+                }).join("")}
+
+            </div>
+
+        </section>
+
+    `;
 }
