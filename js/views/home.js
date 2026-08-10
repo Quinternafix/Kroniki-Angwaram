e045483b616ceadc629d9ddb08bd68e35d1ff894
Quinternafix <quint4022@gmail.com>
2026-08-10 18:11:06 +0200

    Update home.js

--- js/views/home.js
+++ js/views/home.js
@@ -1,9 +1,34 @@
 import { t } from "../core/i18n.js";
 
 export function homeView() {
-  return `<section class="hero">
-    <h1>${t("site.title")}</h1>
-    <p>${t("home.welcome")}</p>
-    <p>${t("home.instructions")}</p>
-  </section>`;
+
+    return `
+
+        <section class="hero">
+
+            <div class="home-logo">
+
+                <img
+                    src="assets/KronikiAngwaram.png"
+                    alt="${t("site.title")}"
+                >
+
+            </div>
+
+            <h1>
+                ${t("site.title")}
+            </h1>
+
+            <p>
+                ${t("home.welcome")}
+            </p>
+
+            <p>
+                ${t("home.instructions")}
+            </p>
+
+        </section>
+
+    `;
+
 }
