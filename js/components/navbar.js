bdd59893e81a11e0241e4c4f9ed3c4e1ed8d32ab
Quinternafix <quint4022@gmail.com>
2026-09-21 19:27:12 +0200

    Update navbar.js

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -158,3 +158,5 @@ export function highlightCurrentPage() {
 
         });
 }
+window.addEventListener("hashchange", highlightCurrentPage);
+window.addEventListener("DOMContentLoaded", highlightCurrentPage);
