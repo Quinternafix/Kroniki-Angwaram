b8f9b67509a1d8415324ba475a657431df2d06f8
Quinternafix <quint4022@gmail.com>
2026-09-21 19:20:06 +0200

    Add event listeners for current page highlighting
    Added event listeners to highlight the current page on hash change and DOM content load.

--- components/navbar.js
+++ components/navbar.js
@@ -169,3 +169,5 @@ export function highlightCurrentPage() {
 
         });
 }
+window.addEventListener("hashchange", highlightCurrentPage);
+window.addEventListener("DOMContentLoaded", highlightCurrentPage);
